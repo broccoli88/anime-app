237ff6c7042fc12c5fe096f7d81ab68c21d5e1de
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-05-06 16:33:34 +0200

    fix with initial fetch on page load

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,14 @@
 	import { reactive, ref, watch, inject, onMounted } from "vue";
 
 	const animeList = reactive({ list: [] });
-	const animeGenres = reactive({ list: [] });
+	const animeRanking = reactive({ list: [] });
 	const searchAnime = inject("search-anime");
 	const isFetching = inject("fetching");
+	const isSearching = ref(false);
 
 	const fetchAnimeList = async () => {
-		const url = `https://anime-db.p.rapidapi.com/anime?page=1&size=10&search=${searchAnime.value}&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc`;
+		isSearching.value = true;
+		const url = `https://anime-db.p.rapidapi.com/anime?page=1&size=12&search=${searchAnime.value}&genres=Fantasy%2CDrama&sortBy=ranking&sortOrder=asc`;
 		const options = {
 			method: "GET",
 			headers: {
@@ -25,23 +27,23 @@
 	};
 
 	onMounted(async () => {
-		for (let i = 1; i <= 10; i++) {
-			const url = `https://anime-db.p.rapidapi.com/anime/by-ranking/${i}`;
-			const options = {
-				method: "GET",
-				headers: {
-					"X-RapidAPI-Key":
-						"c711947e3amsh0c697f08f7842e1p1f50d7jsndcb62addaea8",
-					"X-RapidAPI-Host": "anime-db.p.rapidapi.com",
-				},
-			};
+		isSearching.value = false;
+		const url =
+			"https://anime-db.p.rapidapi.com/anime?page=1&size=10&sortBy=ranking";
 
-			const { response, fetchData } = useFetch(url, options);
+		const options = {
+			method: "GET",
+			headers: {
+				"X-RapidAPI-Key":
+					"c711947e3amsh0c697f08f7842e1p1f50d7jsndcb62addaea8",
+				"X-RapidAPI-Host": "anime-db.p.rapidapi.com",
+			},
+		};
 
-			await fetchData();
-			animeGenres.list.push(response.value);
-		}
-		console.log(animeGenres.list);
+		const { response, fetchData } = useFetch(url, options);
+
+		await fetchData();
+		animeRanking.list = response.value.data;
 	});
 
 	watch(isFetching, () => {
@@ -53,10 +55,28 @@
 <template>
 	<section
 		class="main-view"
-		v-if="animeList.list.length > 0 && animeList.list !== null"
+		v-if="
+			animeList.list.length > 0 && animeList.list !== null && isSearching
+		"
 	>
+		<h1>Search results for: {{ searchAnime }}</h1>
 		<AnimeCard
 			v-for="anime in animeList.list"
+			:key="anime._id"
+			:anime="anime"
+		/>
+	</section>
+	<section
+		class="main-view"
+		v-else-if="
+			animeRanking.list.length > 0 &&
+			animeRanking.list !== null &&
+			!isSearching
+		"
+	>
+		<h1>Anime Ranking top 10:</h1>
+		<AnimeCard
+			v-for="anime in animeRanking.list"
 			:key="anime._id"
 			:anime="anime"
 		/>
@@ -71,5 +91,11 @@
 
 		gap: $gap-sm;
 		padding: $padding-md;
+
+		h1 {
+			display: block;
+			grid-column: 1 / -1;
+			margin-bottom: $margin-lg;
+		}
 	}
 </style>
